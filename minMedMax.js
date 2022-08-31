469c05c495ff594f61d1072dff8030e4575f6638
Jerome <naronkrach@gmail.com>
2022-08-31 20:06:07 +0700

    Editted method (Sry, didn't see the หมายเหตุ)

--- minMedMax.js
+++ minMedMax.js
@@ -1,10 +1,15 @@
 const { template } = require("@babel/core");
 
 function minMedMax(n1, n2, n3) {
+  let min = n1;
+  let med = n1;
+  let max = n1;
+  n2 < min ? (min = n2) : n2 > max ? (max = n2) : (med = n2);
+  n3 < min ? (min = n3) : n3 > max ? (max = n3) : (med = n3);
   return {
-    min: Math.min(n1, n2, n3),
-    mid: Math.median(n1, n2, n3),
-    max: Math.max(n1, n2, n3),
+    min: min,
+    mid: med,
+    max: max,
   };
 }
 module.exports = minMedMax;
