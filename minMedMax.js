7e3b1bd31784aa6adea1f218f76a745ad681567a
Jerome <naronkrach@gmail.com>
2022-08-31 20:29:59 +0700

    Changed method

--- minMedMax.js
+++ minMedMax.js
@@ -1,11 +1,11 @@
 const { template } = require("@babel/core");
 
 function minMedMax(n1, n2, n3) {
-  let min = n1;
-  let med = n1;
-  let max = n1;
-  n2 < min ? (min = n2) : n2 > max ? (max = n2) : (med = n2);
-  n3 < min ? (min = n3) : n3 > max ? (max = n3) : (med = n3);
+  let params = [n1, n2, n3];
+  let min = n1 < n2 && n1 < n3 ? n1 : n2 < n3 ? n2 : n3;
+  let max = n1 > n2 && n1 > n3 ? n1 : n2 > n3 ? n2 : n3;
+  let med = params.forEach((n) => n !== min && n !== max && (med = n));
+
   return {
     min: min,
     mid: med,
