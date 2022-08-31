ee10ed2052ca2f5339c4581614e115dbfe6f366f
Jerome <naronkrach@gmail.com>
2022-08-31 22:09:30 +0700

    Using splice to find med

--- minMedMax.js
+++ minMedMax.js
@@ -1,12 +1,12 @@
 const { template } = require("@babel/core");
 
 function minMedMax(n1, n2, n3) {
-  let params = [n1, n2, n3];
   let min = n1 < n2 && n1 < n3 ? n1 : n2 < n3 ? n2 : n3;
   let max = n1 > n2 && n1 > n3 ? n1 : n2 > n3 ? n2 : n3;
-  let med;
-  params.forEach((n) => n !== min && n !== max && (med = n));
-
+  let params = [n1, n2, n3];
+  params.splice(params.indexOf(min), 1);
+  params.splice(params.indexOf(max), 1);
+  let med = params[0];
 
   return {
     min: min,
