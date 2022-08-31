12cdd0e99033babe4161e522eb51436262369ca1
Jerome <naronkrach@gmail.com>
2022-08-31 19:56:31 +0700

    Done minmedmax (first try)

--- minMedMax.js
+++ minMedMax.js
@@ -1,6 +1,10 @@
-const { template } = require('@babel/core')
+const { template } = require("@babel/core");
 
 function minMedMax(n1, n2, n3) {
-  //code here
+  return {
+    min: Math.min(n1, n2, n3),
+    mid: Math.median(n1, n2, n3),
+    max: Math.max(n1, n2, n3),
+  };
 }
-module.exports = minMedMax
+module.exports = minMedMax;
