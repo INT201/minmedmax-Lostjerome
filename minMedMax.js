49e4f250c550275e1bb4ae771f2323a6b64e4ffa
Jerome <naronkrach@gmail.com>
2022-08-31 20:31:22 +0700

    Fixed error

--- minMedMax.js
+++ minMedMax.js
@@ -4,7 +4,9 @@ function minMedMax(n1, n2, n3) {
   let params = [n1, n2, n3];
   let min = n1 < n2 && n1 < n3 ? n1 : n2 < n3 ? n2 : n3;
   let max = n1 > n2 && n1 > n3 ? n1 : n2 > n3 ? n2 : n3;
-  let med = params.forEach((n) => n !== min && n !== max && (med = n));
+  let med;
+  params.forEach((n) => n !== min && n !== max && (med = n));
+
 
   return {
     min: min,
